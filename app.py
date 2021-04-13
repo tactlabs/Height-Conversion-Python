b57e882e0daf7ebff150e26017f96699e4e1c297
Sam Richard <josephiterichard0411@gmail.com>
2021-04-13 20:38:28 +0530

    successfully working

--- app.py
+++ app.py
@@ -3,8 +3,9 @@ from flask import Flask, request, render_template
   
 # Flask constructor
 app = Flask(__name__)  
+# port
 PORT = 7000
-  
+
 @app.route('/', methods =["GET", "POST"])
 def gfg():
     if request.method == "POST":
@@ -16,6 +17,6 @@ def gfg():
        result= float(x)*float(y)
        return "Your Height is "+ str(result)+ " in cm"
     return render_template("index.html")
-  
+
 if __name__=='__main__':
    app.run(debug = True,host="0.0.0.0",port = PORT)
